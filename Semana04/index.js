0a2c333d834e8b536910bf26a1c78e21690a9dcc
Jorge Herrera <jorgeluishm88@gmail.com>
2024-06-20 15:34:16 -0300

    Semana4

---
+++ Semana04/index.js
@@ -0,0 +1,9 @@
+function highlight(element) {
+  element.style.transform = "scale(1.1)";
+  element.style.transition = "transform 0.3s";
+}
+
+function unhighlight(element) {
+  element.style.transform = "scale(1)";
+}
+//////////////////////////////////////////////
